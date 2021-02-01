e532d8c418ea63453a32e005db796e071ade3577
dmitrydao <dmitrydaopro@gmail.com>
2021-02-01 16:23:41 +0200

    [Update]: move configs into .env

--- graph-queries/aggregator.js
+++ graph-queries/aggregator.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
+require('dotenv').config();
+
 const main = async () => {
   try {
-    const ensApi = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens';
-    const resolverAddress = '0x122eb74f9d0f1a5ed587f43d120c1c2bbdb9360b';
-    const pairId = '0xfb3362a97947a1804738d871b660275dff443b48fb7da009b2c605969b2045e9';
+    const ensApi = process.env.ENS_API;
+    const resolverAddress = process.env.RESOLVER_ADDRESS;
+    const pairId = process.env.PAIR_ID;
     const domainId = resolverAddress.concat('-', pairId);
 
     const result = await axios.post(ensApi,
